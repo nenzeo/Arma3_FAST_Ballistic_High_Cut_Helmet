432764f7f971391a5534268562259963038561f1
Stig Røsnes <androidstigrr@gmail.com>
2018-04-30 10:36:47 +0200

    first commit

---
+++ config.cpp
@@ -0,0 +1,10 @@
+#include "basicdefines_A3.hpp"
+class DefaultEventhandlers;
+#include "cfgPatches.hpp"
+class UniformSlotInfo;
+class cfgWeapons
+{
+	class InventoryItem_Base_F;
+	class ItemCore;
+	#include "cfgHeadgear.hpp"	/// sample cap is defined in a separate file to make it cleaner
+};
